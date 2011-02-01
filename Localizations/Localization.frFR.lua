a1c4b72aa631c3606b2dbe28684cf018238d1562
HgAlexx <alex@mzhg.space>
2011-02-01 02:01:40 +0100

    * Fix debug code into Fr Localization

--- Localizations/Localization.frFR.lua
+++ Localizations/Localization.frFR.lua
@@ -1,28 +1,4 @@
--- local addonName, PPBEC_Localization = ...;
-
 if (GetLocale() == "frFR") then
---@debug@
-PPBEC_Localization["FT_Version"] = "Version: %s"
-PPBEC_Localization["PPBEC/ConfigPanel/BuffPerLine/Desc"] = "Combien d'am\195\169lioration voulez-vous par ligne ?"
-PPBEC_Localization["PPBEC/ConfigPanel/BuffPerLine/Name"] = "Am\195\169lioration par ligne"
-PPBEC_Localization["PPBEC/ConfigPanel/HandleWeaponBuff/Desc"] = "D\195\169place les enchantements d'armes sous le portrait du joueur de la m\195\170me mani\195\168re que les am\195\169liorations."
-PPBEC_Localization["PPBEC/ConfigPanel/HandleWeaponBuff/Name"] = "G\195\169rer les enchantements d'armes"
-PPBEC_Localization["PPBEC/ConfigPanel/ShowNativeCoolDown/Desc"] = "Ajoute l'effet visuel de cooldown sur les am\195\169liorations et les affaiblissements, le r\195\169sultat est meilleur si vous l'utilisez avec un autre addon tel que OmniCC."
-PPBEC_Localization["PPBEC/ConfigPanel/ShowNativeCoolDown/Name"] = "Affiche les cooldown natifs"
-PPBEC_Localization["PPBEC/ConfigPanel/ShowOriginalTextTimer/Desc"] = "Affiche la dur\195\169e de l'am\195\169lioration en dessous de l'icone."
-PPBEC_Localization["PPBEC/ConfigPanel/ShowOriginalTextTimer/Name"] = "Affiche les textes de dur\195\169es"
-PPBEC_Localization["PPBEC/ConfigPanel/ShowSecondUnder10m/Desc"] = "Si les dur\195\169es des am\195\169liorations sont affich\195\169s, et si m\195\170me si vous avez coch\195\169 \"Hide Seconds\" dans les options de Perl, en dessous de 10 minutes elles seront tout de m\195\170me affich\195\169es."
-PPBEC_Localization["PPBEC/ConfigPanel/ShowSecondUnder10m/Name"] = "Affiche les secondes en dessous de 10 minutes"
-PPBEC_Localization["PPBEC/ConfigPanel/TitleDesc"] = "Changez tous les nouveaux r\195\169glages ici."
-PPBEC_Localization["PPBEC/ConfigPanel/VerticalSpacing/Desc"] = "Espacement verticale entre les lignes d'am\195\169liorations et affaiblissements"
-PPBEC_Localization["PPBEC/ConfigPanel/VerticalSpacing/Name"] = "Espacement verticale"
-PPBEC_Localization["PPBEC/ConfigPanel/XOffset/Desc"] = "D\195\169calage horizontale relatif \195\160 la fen\195\170tre m\195\168re."
-PPBEC_Localization["PPBEC/ConfigPanel/XOffset/Name"] = "D\195\169calage horizontale"
-PPBEC_Localization["PPBEC/ConfigPanel/YOffset/Desc"] = "D\195\169calage verticale relatif \195\160 la fen\195\170tre m\195\168re."
-PPBEC_Localization["PPBEC/ConfigPanel/YOffset/Name"] = "D\195\169calage verticale"
-PPBEC_Localization["PPBEC/ToolTip/LeftButtonDesc"] = "|cFFFFFFFF <Clique gauche>|r Ouvre le panneau d'options de \"Perl Classic Unit Frames\"|r"
-PPBEC_Localization["PPBEC/ToolTip/RightButtonDesc"] = "|cFFFFFFFF <Clique droit>|r Ouvre le panneau d'options avanc\195\169es de \"Perl Player Buff\"|r"
---@end-debug@
 
 --@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="english", table-name="PPBEC_Localization")@
 
