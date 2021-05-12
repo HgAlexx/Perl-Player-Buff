6e63b6e16d39293c6f4e687a744e408133726351
HgAlexx <alex@mzhg.space>
2021-05-12 11:04:19 +0200

    * Update Classic toc version
    * Preparation for TBC

--- Utility.lua
+++ Utility.lua
@@ -16,14 +16,14 @@ do
 
     Utility.Mode = 1
     Utility.IsClassic = false
-    --Utility.IsTBC = false
+    Utility.IsTBC = false
     Utility.IsRetail = false
 
     local _, _, _, interfaceVersion = GetBuildInfo()
-    if interfaceVersion >= 10000 and interfaceVersion < 20000 then
+    if interfaceVersion >= 10000 and interfaceVersion < 20500 then
         Utility.IsClassic = true
-    --elseif interfaceVersion >= 20000 and interfaceVersion < 30000 then
-    --    Utility.IsTBC = true
+    elseif interfaceVersion >= 20500 and interfaceVersion < 30000 then
+        Utility.IsTBC = true
     elseif interfaceVersion >= 90000 then
         Utility.IsRetail = true
     end
